eb997c790ba0d739b22a5ecab95a7b86441c1c01
dmc <david.mark.clements@gmail.com>
2025-08-08 18:23:11 +0200

    🍐🎭

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+'use strict'
+const test = require('brittle')
+const { ALIASES, EOLS } = require('..')
+test('ALIASES contains key buffers', (t) => {
+  t.ok(Object.values(ALIASES).every((key) => key instanceof Buffer))
+})
+test('EOLS contains arrays of buffers', (t) => {
+  t.ok(Object.values(EOLS).every((arr) => Array.isArray(arr) && arr.every((key) => key instanceof Buffer)))
+})
