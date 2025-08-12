5ef992561170aaec96fd511a4b2d9b65ca1489f6
dmc <david.mark.clements@gmail.com>
2025-08-12 15:19:45 +0200

    pear://keet to prod alias

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const hypercoreid = require('hypercore-id-encoding')
 
 exports.ALIASES = {
   pear: hypercoreid.decode('pqbzjhqyonxprx8hghxexnmctw75mr91ewqw5dxe1zmntfyaddqy'),
-  keet: hypercoreid.decode('ma9zo8zmfat3ih314mne1q47shrfho1odyfm9methgnn7b866w6y'),
+  keet: hypercoreid.decode('oeeoz3w6fjjt7bym3ndpa6hhicm8f8naxyk11z4iypeoupn6jzpo'),
   runtime: hypercoreid.decode('rbw6fbxorqgjgyitworh3f73utc5cu7sczhptn64oonbznuojiao'),
   doctor: hypercoreid.decode('ouenymy889n4ri9g74jm5bcr46wc6hqnxadqhfmbc4xbskerehby'),
   electron: hypercoreid.decode('goowesg5dga9j1ryx47rsk9o4zms4541me4zerxsnbu8u99duh4o')
 }
 
 exports.EOLS = {
-  keet: [hypercoreid.decode('jc38t9nr7fasay4nqfxwfaawywfd3y14krnsitj67ymoubiezqdy'), hypercoreid.decode('oeeoz3w6fjjt7bym3ndpa6hhicm8f8naxyk11z4iypeoupn6jzpo')]
+  keet: [hypercoreid.decode('jc38t9nr7fasay4nqfxwfaawywfd3y14krnsitj67ymoubiezqdy')]
 }
