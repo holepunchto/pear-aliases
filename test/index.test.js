68692f9bafcc56ff5d14f9ba771b5924c964ff8f
Mikkel Malmberg <mikkel@brnbw.com>
2025-09-22 14:37:26 +0200

    Apply Prettier

--- test/index.test.js
+++ test/index.test.js
@@ -5,5 +5,9 @@ test('ALIASES contains key buffers', (t) => {
   t.ok(Object.values(ALIASES).every((key) => key instanceof Buffer))
 })
 test('EOLS contains arrays of buffers', (t) => {
-  t.ok(Object.values(EOLS).every((arr) => Array.isArray(arr) && arr.every((key) => key instanceof Buffer)))
+  t.ok(
+    Object.values(EOLS).every(
+      (arr) => Array.isArray(arr) && arr.every((key) => key instanceof Buffer)
+    )
+  )
 })
